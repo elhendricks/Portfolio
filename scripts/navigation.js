17dfb98dc5c9977e3e46a671033810147e04b615
elhendricks <elhendricks@gmail.com>
2016-09-29 15:29:53 -0700

    added IIFE to both js files

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,65 +1,68 @@
-var pageNavigation = {};
+(function(module){
+  var pageNavigation = {};
 
 
-pageNavigation.handleTabs = function() {
-  //on click of
-  //tab $('.tab')
-  //get the value of data-section attr
-  //hide all the other content on the page
-  //display only the tab with an id === data section attr
-  //when page loads .click() first tab
-  $('#main-nav').on('click', '.tab', function(event){
-    event.preventDefault();
-    var $tabName = $(this).attr('data-section');
-    $('.tab-section').hide();
-    $('#' + $tabName).show();
+  pageNavigation.handleTabs = function() {
+    //on click of
+    //tab $('.tab')
+    //get the value of data-section attr
+    //hide all the other content on the page
+    //display only the tab with an id === data section attr
+    //when page loads .click() first tab
+    $('#main-nav').on('click', '.tab', function(event){
+      event.preventDefault();
+      var $tabName = $(this).attr('data-section');
+      $('.tab-section').hide();
+      $('#' + $tabName).show();
 
+    });
+
+    $('#main-nav .tab:first').click();
+  };
+
+  pageNavigation.handleFilters = function() {
+
+  };
+
+  pageNavigation.toggleProjectLength = function() {
+    $('.project-description *:nth-of-type(n+2)').hide();
+
+    var descriptionShown = false;
+    $('#projects').on('click', '.read-toggle', function(event) {
+      event.preventDefault();
+      if (!descriptionShown) {
+        $(this).siblings('.project-description').find('*').show();
+        $(this).text('<< Show Less');
+        descriptionShown = true;
+      } else {
+        $('.project-description *:nth-of-type(n+2)').hide();
+        $(this).text('Read More >>');
+        descriptionShown = false;
+      }
+    });
+
+  };
+
+  pageNavigation.handleProjects = function() { Project.all.forEach(function(a)
+    {
+    $('#projects').append(a.toHtml());
   });
+  };
 
-  $('#main-nav .tab:first').click();
-};
+  pageNavigation.handleCodeLines = function () {
+    $('#projects').append('<h3>Erica has written '+ Project.countLines() +' lines of code--which is in no way a fabricated number.');
+  };
 
-pageNavigation.handleFilters = function() {
+  //invoke the functions
+  pageNavigation.renderPage = function() {
 
-};
+    pageNavigation.handleTabs();
+    pageNavigation.handleProjects();
+    pageNavigation.toggleProjectLength();
+    pageNavigation.handleCodeLines();
+  };
 
-pageNavigation.toggleProjectLength = function() {
-  $('.project-description *:nth-of-type(n+2)').hide();
-
-  var descriptionShown = false;
-  $('#projects').on('click', '.read-toggle', function(event) {
-    event.preventDefault();
-    if (!descriptionShown) {
-      $(this).siblings('.project-description').find('*').show();
-      $(this).text('<< Show Less');
-      descriptionShown = true;
-    } else {
-      $('.project-description *:nth-of-type(n+2)').hide();
-      $(this).text('Read More >>');
-      descriptionShown = false;
-    }
-  });
-
-};
-
-pageNavigation.handleProjects = function() { Project.all.forEach(function(a)
-  {
-  $('#projects').append(a.toHtml());
-});
-};
-
-pageNavigation.handleCodeLines = function () {
-  $('#projects').append('<h3>Erica has written '+ Project.countLines() +' lines of code--which is in no way a fabricated number.');
-};
-
-//invoke the functions
-pageNavigation.renderPage = function() {
-
-  pageNavigation.handleTabs();
-  pageNavigation.handleProjects();
-  pageNavigation.toggleProjectLength();
-  pageNavigation.handleCodeLines();
-};
-
-Project.fetchProjects(pageNavigation.renderPage);
-// Project.fetchProjects(getThings);
+  Project.fetchProjects(pageNavigation.renderPage);
+  // Project.fetchProjects(getThings);
+  module.pageNavigation = pageNavigation;
+}());
--- scripts/script.js
+++ scripts/script.js
@@ -1,118 +1,120 @@
+(function(module){
+  var filters = [{name: 'title'}, {name: 'language'}];
 
-var filters = [{name: 'title'}, {name: 'language'}];
+  function Project (opts) {
+    this.title = opts.title;
+    this.github = opts.github;
+    this.description = opts.description;
+    this.screenshot = opts.screenshot;
+    this.language = opts.language;
+    this.linesJS = opts.linesJS;
+    this.linesCSS = opts.linesCSS;
+    this.linesHTML = opts.linesHTML;
+  }
 
-function Project (opts) {
-  this.title = opts.title;
-  this.github = opts.github;
-  this.description = opts.description;
-  this.screenshot = opts.screenshot;
-  this.language = opts.language;
-  this.linesJS = opts.linesJS;
-  this.linesCSS = opts.linesCSS;
-  this.linesHTML = opts.linesHTML;
-}
+  Project.all = [];
 
-Project.all = [];
-
-Project.prototype.toHtml = function () {
-  var source = $('#project-template').html();
-  var template = Handlebars.compile(source);
-  var html = template(this);
-  return html;
-};
-
-
-
-function toFilterHtml(f) {
-  var template = Handlebars.compile($('#filter-template').html());
-  var html = template(f);
-  return html;
-};
+  Project.prototype.toHtml = function () {
+    var source = $('#project-template').html();
+    var template = Handlebars.compile(source);
+    var html = template(this);
+    return html;
+  };
 
 
-function toPopulateFilterHtml() {
-  var template = Handlebars.compile($('#category-template')).html();
-  var html = template ();
-  return html;
-};
 
-// function getThings() {
-//   var filterArray = filters.map(function(ele) {
-//     return ele.name;
-//   });
-//
-// console.log("filterArray: " + filterArray);
-//   var categoryArray = filterArray.map(function(item) {
-//     console.log("item: " + item);
-//     return Project.all.map(function(thing) {
-//       console.log("thing[item]: "+ thing[item]);
-//       return {category: thing[item]};
-//     });
-//   });
-// console.log("categoryArray: " + categoryArray);
-//
-// filterArray.forEach(function(){
-//   categoryArray.forEach(function(junk){
-//     console.log("junk.category " + junk.title);
-//     toPopulateFilterHtml(junk);
-//   });
-//
-// })
-// };
-//
-//
-//
-//
-// // console.log(categoryArray);
-//
-//
-//
-//
+  function toFilterHtml(f) {
+    var template = Handlebars.compile($('#filter-template').html());
+    var html = template(f);
+    return html;
+  };
 
-Project.loadProjects = function(data) {
-  Project.all = data.map(function(ele) {
-    return new Project(ele);
+
+  function toPopulateFilterHtml() {
+    var template = Handlebars.compile($('#category-template')).html();
+    var html = template ();
+    return html;
+  };
+
+  // function getThings() {
+  //   var filterArray = filters.map(function(ele) {
+  //     return ele.name;
+  //   });
+  //
+  // console.log("filterArray: " + filterArray);
+  //   var categoryArray = filterArray.map(function(item) {
+  //     console.log("item: " + item);
+  //     return Project.all.map(function(thing) {
+  //       console.log("thing[item]: "+ thing[item]);
+  //       return {category: thing[item]};
+  //     });
+  //   });
+  // console.log("categoryArray: " + categoryArray);
+  //
+  // filterArray.forEach(function(){
+  //   categoryArray.forEach(function(junk){
+  //     console.log("junk.category " + junk.title);
+  //     toPopulateFilterHtml(junk);
+  //   });
+  //
+  // })
+  // };
+  //
+  //
+  //
+  //
+  // // console.log(categoryArray);
+  //
+  //
+  //
+  //
+
+  Project.loadProjects = function(data) {
+    Project.all = data.map(function(ele) {
+      return new Project(ele);
+    });
+  };
+
+
+
+
+  Project.countLines = function() {
+    return Project.all.map(function(obj) {
+      return obj.linesCSS + obj.linesJS + obj.linesHTML;
+    }).reduce(function(acc, curr){
+      return acc + curr;
+    });
+  };
+
+  Project.fetchProjects = function(next) {
+    $.ajax({
+      method: 'GET',
+      url: '/data/projects.json',
+      error: function() {
+        console.log('There was an error getting the data');
+      },
+      success: function(data) {
+        // var parsedProjects = JSON.parse(data);
+        Project.loadProjects(data);
+        next();
+      }
+    });
+  };
+
+  // Project.listProjects = function() {
+  //   filters.map(
+  //     function(filterObj) {
+  //       var filter = filterObj.name;
+  //       return Project.all.map(function(ProjectObj){
+  //         return ProjectObj.filter;
+  //         // return {filter: };
+  //       });
+  //     });
+  // };
+
+
+  filters.forEach(function(b) {
+    $('#filters').append(toFilterHtml(b));
   });
-};
-
-
-
-
-Project.countLines = function() {
-  return Project.all.map(function(obj) {
-    return obj.linesCSS + obj.linesJS + obj.linesHTML;
-  }).reduce(function(acc, curr){
-    return acc + curr;
-  });
-};
-
-Project.fetchProjects = function(next) {
-  $.ajax({
-    method: 'GET',
-    url: '/data/projects.json',
-    error: function() {
-      console.log('There was an error getting the data');
-    },
-    success: function(data) {
-      // var parsedProjects = JSON.parse(data);
-      Project.loadProjects(data);
-      next();
-    }
-  });
-};
-
-// Project.listProjects = function() {
-//   filters.map(
-//     function(filterObj) {
-//       var filter = filterObj.name;
-//       return Project.all.map(function(ProjectObj){
-//         return ProjectObj.filter;
-//         // return {filter: };
-//       });
-//     });
-// };
-
-
-filters.forEach(function(b) {
-  $('#filters').append(toFilterHtml(b));
-});
+  module.Project = Project;
+}());
